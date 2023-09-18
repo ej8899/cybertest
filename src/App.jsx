51f682bf444537b3eb412304774fa6b2886a97cb
ej <ej8899@gmail.com>
2023-09-18 14:50:10 -0400

    add: navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 
 import './App.css'
 import QuizApp from './QuizApp';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <QuizApp />
     </>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+// Navbar.jsx
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      logo ---
+      
+      <a href="/">Home</a> |&nbsp;
+      <a href="/logout">Logout</a>
+    </nav>
+  );
+}
+
+export default Navbar;
